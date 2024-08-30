c792e9e62a0eebc744d54e5a564f8f8301a40b4c
George Agyemang <geoagye@gmail.com>
2024-08-30 17:04:52 +0000

    28th 2.0

--- resources/views/company/catalog/edit.blade.php
+++ resources/views/company/catalog/edit.blade.php
@@ -2,7 +2,49 @@
 
     @section('title', 'Edit Stock')
     <div class="container-xxl flex-grow-1 container-p-y">
-        
+        <div class="row">
+            <!-- Basic -->
+            <div class="col-md-6 mx-auto">
+                <div class="card mb-4">
+                    <h5 class="card-header">Edit Catalog</h5>
+                    <form method="POST" action="{{ route('catalog.update', ['slug' => $company->slug, 'id' => $catalog->id]) }}"
+                        class="mt-10 space-y-6"
+                        enctype="multipart/form-data">
+                    @csrf
+                    @method('PATCH')
+
+                    <div class="card-body demo-vertical-spacing demo-only-element">
+                        <div class="form-password-toggle">
+                            <label class="form-label" for="basic-default-password12">Stock Name</label>
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="Enter Stock Name" aria-label="Enter Stock Name"
+                                    aria-describedby="basic-addon11" />
+                            </div>
+                        </div>
+
+                        <div class="input-group">
+                            <span class="input-group-text">$</span>
+                            <input type="text" class="form-control" placeholder="Amount"
+                                aria-label="Amount (to the nearest dollar)" />
+                            <span class="input-group-text">.00</span>
+                        </div>
+
+
+
+
+
+
+                        <div class="input-group">
+                            <span class="input-group-text">With textarea</span>
+                            <textarea class="form-control" aria-label="With textarea" placeholder="Comment"></textarea>
+                        </div>
+
+                    </div>
+                    </form>
+                   
+                </div>
+            </div>
+        </div>
 
     </div>
 
