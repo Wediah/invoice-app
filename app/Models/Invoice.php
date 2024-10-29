c922a9d5dbd9374ff3f766af63ced628f8c7d82a
George Agyemang <geoagye@gmail.com>
2024-10-29 13:56:21 +0000

    pushes 4.1

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
-class invoice extends Model
+class Invoice extends Model
 {
     use HasFactory;
     protected $guarded = [];
